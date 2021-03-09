3d7fc6490a0741713e945852c5d8e331a9f4416c
insurecar <rostyslavshyian@gmail.com>
2021-03-09 17:16:41 +0200

    add components with buttons

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Alert } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import { Navbar } from "./src/components/Navbar";
+import { MainScreen } from "./src/screens/MainScreen";
+import { TodoScreen } from "./src/screens/TodoScreen";
+
+async function loadApplication() {
+  await Font.loadAsync({
+    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+}
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const [todoId, setTodoId] = useState(null);
+  const [todos, setTodos] = useState([{ id: "1", title: "Выучить React Native" }]);
+
+  if (!isReady) {
+    return (
+      <AppLoading startAsync={loadApplication} onError={(err) => console.log(err)} onFinish={() => setIsReady(true)} />
+    );
+  }
+
+  const addTodo = (title) => {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title,
+      },
+    ]);
+  };
+
+  const removeTodo = (id) => {
+    const todo = todos.find((t) => t.id === id);
+    Alert.alert(
+      "Удаление элемента",
+      `Вы уверены, что хотите удалить "${todo.title}"?`,
+      [
+        {
+          text: "Отмена",
+          style: "cancel",
+        },
+        {
+          text: "Удалить",
+          style: "destructive",
+          onPress: () => {
+            setTodoId(null);
+            setTodos((prev) => prev.filter((todo) => todo.id !== id));
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const updateTodo = (id, title) => {
+    setTodos((old) =>
+      old.map((todo) => {
+        if (todo.id === id) {
+          todo.title = title;
+        }
+        return todo;
+      })
+    );
+  };
+
+  let content = <MainScreen todos={todos} addTodo={addTodo} removeTodo={removeTodo} openTodo={setTodoId} />;
+
+  if (todoId) {
+    const selectedTodo = todos.find((todo) => todo.id === todoId);
+    content = (
+      <TodoScreen onRemove={removeTodo} goBack={() => setTodoId(null)} todo={selectedTodo} onSave={updateTodo} />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title="Todo App!" />
+      <View style={styles.container}>{content}</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
 });
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { THEME } from '../theme'
+import { AppTextBold } from './ui/AppTextBold'
+
+export const Navbar = ({ title }) => {
+  return (
+    <View style={styles.navbar}>
+      <AppTextBold style={styles.text}>{title}</AppTextBold>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  navbar: {
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: THEME.MAIN_COLOR,
+    paddingBottom: 10
+  },
+  text: {
+    color: 'white',
+    fontSize: 20
+  }
+})
